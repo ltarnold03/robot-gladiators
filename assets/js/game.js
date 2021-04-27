8562cf7ec5ef090f4e7b6225c26bed66dfbcaee6
LaMonica_Arnold <ltarnold03@yahoo.com>
2021-04-26 22:31:28 -0500

    add js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert ("This is an aler! JavaScript is running!");
+var playerName = window.prompt("What's your robot's name?");
+// Note the lack of quotation amrks around playerName window.alert(playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight ();
