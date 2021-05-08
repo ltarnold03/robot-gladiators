dd7e78e8ebb44ce947a2edd8134d59b1fed31795
LaMonica_Arnold <ltarnold03@yahoo.com>
2021-05-08 00:25:39 -0500

    add highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -148,7 +148,24 @@ var endGame = function() {
     else {
         window.alert("The game has now ended. Let's see how you did!");
     }
-    // ask player if they'd like to p;ay again
+
+    // check localStorage for highscore, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+    // if player has more money thatn the high score, play has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.mone + "!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }    
+
+    // ask player if they'd like to play again
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if (playAgainConfirm) {
